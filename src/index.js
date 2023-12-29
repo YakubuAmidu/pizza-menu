a8e3fa0889cd2a6f4aa89cefe4760e71e965a62e
YakubuAmidu <yakubu.amidu@yahoo.com>
2023-12-28 23:13:07 -0800

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 const pizzaData = [
   {
@@ -48,7 +49,7 @@ const pizzaData = [
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
       <Menu />
       <Footer />
@@ -57,21 +58,35 @@ function App() {
 }
 
 function Header() {
-  return <h1>Fast React Pizza Co.</h1>;
+  const style = {};
+
+  return (
+    <header className="header">
+      <h1 styles={style}>Fast React Pizza Co.</h1>;
+    </header>
+  );
 }
 
 function Menu() {
   return (
-    <div>
+    <main className="menu">
       <h2>Our Menu</h2>
       <Pizza />
-    </div>
+      <Pizza />
+      <Pizza />
+    </main>
   );
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log("Open: ", isOpen);
+
   return (
-    <footer>
+    <footer className="footer">
       {new Date().toLocaleTimeString()}. We're currently open...😔
     </footer>
   );
@@ -81,7 +96,7 @@ function Pizza() {
   return (
     <div>
       <img src="pizzas/spinaci.jpg" alt="pizza spinaci" />
-      <h2>Pizza spinaci</h2>
+      <h3>Pizza spinaci</h3>
       <p>Tomato, mozarella, spinach and recotta cheese</p>
     </div>
   );
